1826f96bcfc3d9c5df8d202783876b9476f7fab6
John Pham <johnphammail@gmail.com>
2019-12-01 21:15:44 -0800

    Solve part 1

---
+++ day2/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+from typing import List
+
+
+def read_input(filename: str) -> List[str]:
+    lines = []
+
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.rstrip('\n'))
+    return lines
+
+
+input_data = read_input('./input')
+
+END_CODE = 99
+ADD_CODE = 1
+MULTIPLY_CODE = 2
+OFFSET = 4
+
+operations = input_data[0].split(',')
+LENGTH = len(operations)
+
+operations[1] = 12
+operations[2] = 2
+
+print(operations)
+index = 0
+while index < LENGTH:
+    operation = int(operations[index])
+    a_index = int(operations[index + 1])
+    b_index = int(operations[index + 2])
+    c_index = int(operations[index + 3])
+
+    a = int(operations[a_index])
+    b = int(operations[b_index])
+
+    if operation == ADD_CODE:
+        operations[c_index] = a + b
+    elif operation == MULTIPLY_CODE:
+        operations[c_index] = a * b
+    elif operation == END_CODE:
+        print("END")
+        break
+    index += OFFSET
+
+print(operations)
