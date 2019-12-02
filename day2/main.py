143fb3efafe65d3f5d0b099c3d5b8d37047468d2
John Pham <johnphammail@gmail.com>
2019-12-01 21:46:44 -0800

    Finish part 2

--- day2/main.py
+++ day2/main.py
@@ -19,27 +19,65 @@ MULTIPLY_CODE = 2
 OFFSET = 4
 
 operations = input_data[0].split(',')
-LENGTH = len(operations)
+# LENGTH = len(operations)
 
-operations[1] = 12
-operations[2] = 2
+# operations[1] = 12
+# operations[2] = 2
 
-index = 0
-while index < LENGTH:
-    operation = int(operations[index])
-    a_index = int(operations[index + 1])
-    b_index = int(operations[index + 2])
-    c_index = int(operations[index + 3])
+# index = 0
+# while index < LENGTH:
+#     operation = int(operations[index])
+#     a_index = int(operations[index + 1])
+#     b_index = int(operations[index + 2])
+#     c_index = int(operations[index + 3])
 
-    a = int(operations[a_index])
-    b = int(operations[b_index])
+#     a = int(operations[a_index])
+#     b = int(operations[b_index])
 
-    if operation == ADD_CODE:
-        operations[c_index] = a + b
-    elif operation == MULTIPLY_CODE:
-        operations[c_index] = a * b
-    elif operation == END_CODE:
-        break
-    index += OFFSET
+#     if operation == ADD_CODE:
+#         operations[c_index] = a + b
+#     elif operation == MULTIPLY_CODE:
+#         operations[c_index] = a * b
+#     elif operation == END_CODE:
+#         break
+#     index += OFFSET
 
-print('Part 1', operations[0])
+# print('Part 1', operations[0])
+
+test_noun = 0
+test_verb = 0
+
+original_operations = operations.copy()
+for test_noun in range(100):
+    for test_verb in range(100):
+        operations[1] = test_noun
+        operations[2] = test_verb
+
+        index = 0
+        LENGTH = len(operations)
+        while index < LENGTH:
+            operation = int(operations[index])
+            a_index = int(operations[index + 1])
+            b_index = int(operations[index + 2])
+            c_index = int(operations[index + 3])
+
+            a = int(operations[a_index])
+            b = int(operations[b_index])
+
+            if operation == ADD_CODE:
+                operations[c_index] = a + b
+            elif operation == MULTIPLY_CODE:
+                operations[c_index] = a * b
+            elif operation == END_CODE:
+                break
+
+            if operations[0] == 19690720:
+                print(100 * test_noun + test_verb)
+                break
+
+            index += OFFSET
+
+        operations = original_operations.copy()
+
+
+# print('Part 1', operations[0])
